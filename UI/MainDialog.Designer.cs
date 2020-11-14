6d8515a4f9dcf23e89942ba60e63839c156f7452
huikamlam <patrickhui2013047@gmail.com>
2020-11-14 20:37:28 +0800

    Basic layout

--- UI/MainDialog.Designer.cs
+++ UI/MainDialog.Designer.cs
@@ -30,33 +30,235 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainDialog));
             this.FilePanel = new System.Windows.Forms.Panel();
+            this.FileBrowseButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.FileLabel = new System.Windows.Forms.Label();
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.HashPanel = new System.Windows.Forms.Panel();
+            this.Hash = new System.Windows.Forms.Button();
+            this.HashLabel = new System.Windows.Forms.Label();
+            this.HashBox = new System.Windows.Forms.TextBox();
+            this.TextCheck = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TextPanel = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.FilePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.HashPanel.SuspendLayout();
+            this.TextPanel.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // FilePanel
             // 
+            this.FilePanel.Controls.Add(this.FileBrowseButton);
             this.FilePanel.Controls.Add(this.textBox1);
-            this.FilePanel.Controls.Add(this.label1);
+            this.FilePanel.Controls.Add(this.FileLabel);
             resources.ApplyResources(this.FilePanel, "FilePanel");
             this.FilePanel.Name = "FilePanel";
+            // 
+            // FileBrowseButton
+            // 
+            resources.ApplyResources(this.FileBrowseButton, "FileBrowseButton");
+            this.FileBrowseButton.Name = "FileBrowseButton";
+            this.FileBrowseButton.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
             resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
             // 
-            // label1
+            // FileLabel
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.FileLabel, "FileLabel");
+            this.FileLabel.Name = "FileLabel";
+            // 
+            // SplitContainer
+            // 
+            resources.ApplyResources(this.SplitContainer, "SplitContainer");
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.panel5);
+            this.SplitContainer.Panel1.Controls.Add(this.panel4);
+            this.SplitContainer.Panel1.Controls.Add(this.panel3);
+            this.SplitContainer.Panel1.Controls.Add(this.panel2);
+            this.SplitContainer.Panel1.Controls.Add(this.panel1);
+            // 
+            // SplitContainer.Panel2
+            // 
+            resources.ApplyResources(this.SplitContainer.Panel2, "SplitContainer.Panel2");
+            this.SplitContainer.Panel2.Controls.Add(this.HashPanel);
+            // 
+            // HashPanel
+            // 
+            this.HashPanel.Controls.Add(this.Hash);
+            this.HashPanel.Controls.Add(this.HashLabel);
+            this.HashPanel.Controls.Add(this.HashBox);
+            resources.ApplyResources(this.HashPanel, "HashPanel");
+            this.HashPanel.Name = "HashPanel";
+            // 
+            // Hash
+            // 
+            resources.ApplyResources(this.Hash, "Hash");
+            this.Hash.Name = "Hash";
+            this.Hash.UseVisualStyleBackColor = false;
+            // 
+            // HashLabel
+            // 
+            resources.ApplyResources(this.HashLabel, "HashLabel");
+            this.HashLabel.Name = "HashLabel";
+            // 
+            // HashBox
+            // 
+            resources.ApplyResources(this.HashBox, "HashBox");
+            this.HashBox.Name = "HashBox";
+            // 
+            // TextCheck
+            // 
+            resources.ApplyResources(this.TextCheck, "TextCheck");
+            this.TextCheck.Name = "TextCheck";
+            this.TextCheck.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // TextPanel
+            // 
+            this.TextPanel.Controls.Add(this.textBox2);
+            this.TextPanel.Controls.Add(this.TextCheck);
+            resources.ApplyResources(this.TextPanel, "TextPanel");
+            this.TextPanel.Name = "TextPanel";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.textBox7);
+            this.panel5.Controls.Add(this.checkBox5);
+            resources.ApplyResources(this.panel5, "panel5");
+            this.panel5.Name = "panel5";
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            // 
+            // checkBox5
+            // 
+            resources.ApplyResources(this.checkBox5, "checkBox5");
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox6);
+            this.panel4.Controls.Add(this.checkBox4);
+            resources.ApplyResources(this.panel4, "panel4");
+            this.panel4.Name = "panel4";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            // 
+            // checkBox4
+            // 
+            resources.ApplyResources(this.checkBox4, "checkBox4");
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.checkBox3);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // checkBox3
+            // 
+            resources.ApplyResources(this.checkBox3, "checkBox3");
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.checkBox2);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.checkBox1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
             // 
             // MainDialog
             // 
             this.AllowDrop = true;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.SplitContainer);
+            this.Controls.Add(this.TextPanel);
             this.Controls.Add(this.FilePanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -66,7 +268,26 @@
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainDialog_DragEnter);
             this.FilePanel.ResumeLayout(false);
             this.FilePanel.PerformLayout();
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.HashPanel.ResumeLayout(false);
+            this.HashPanel.PerformLayout();
+            this.TextPanel.ResumeLayout(false);
+            this.TextPanel.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,7 +295,32 @@
 
         private System.Windows.Forms.Panel FilePanel;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.Button FileBrowseButton;
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.Panel HashPanel;
+        private System.Windows.Forms.Button Hash;
+        private System.Windows.Forms.Label HashLabel;
+        private System.Windows.Forms.TextBox HashBox;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox TextCheck;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel TextPanel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
